dcf5fe658107a9bcf5949569586802a471be3d37
Code <59381987+c-o-de@users.noreply.github.com>
2023-08-31 17:53:56 +0600

    Add files via upload

---
+++ text_file_encry.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    FILE *in, *out;
+    char ch;
+    in =fopen("in.txt","r");
+    if (in == NULL){
+        puts("File read error");
+        exit(1);
+    }
+
+    out = fopen("out.txt","w");
+    if (out == NULL){
+        puts("File read error");
+        exit(1);
+    }
+
+    do{
+        ch = fgetc(in);
+        fputc(ch-9, out);
+    } while (ch != EOF);
+
+    printf("\nEncryption process completed successfully.");
+    printf("\nout.txt file is generated.");
+
+    fclose(in);
+    fclose(out);
+
+return 0;
+}
